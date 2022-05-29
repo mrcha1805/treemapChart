0eaf0b2c6b96c6999b780266463b1e8ed812db64
Mrcha <31120019+mrcha1805@users.noreply.github.com>
2022-05-29 21:05:16 +0700

    treemap content ui

---
+++ src/app/affinity/affinity.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AffinityComponent } from './affinity.component';
+
+describe('AffinityComponent', () => {
+  let component: AffinityComponent;
+  let fixture: ComponentFixture<AffinityComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AffinityComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AffinityComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AffinityComponent } from './affinity/affinity.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import {MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AffinityComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
